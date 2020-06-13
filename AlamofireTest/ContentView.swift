2f0eda53d1b260df1bf93c2f120a4a3349e5690d
watanabe <watanabe@garyo.biz>
2020-06-13 15:52:01 +0900

    AlamofireとSwiftyJSONを使って、コロナの都道府県毎の死者数をリスト表示する。

--- AlamofireTest/ContentView.swift
+++ AlamofireTest/ContentView.swift
@@ -7,15 +7,59 @@
 //
 
 import SwiftUI
+import Alamofire
+import SwiftyJSON
 
 struct ContentView: View {
+    @ObservedObject var obs = observer()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            List(obs.datas){
+                i in
+                HStack{
+                    Text(i.name_ja)
+                    Text("\(i.deaths)")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct datatype: Identifiable{
+    var id : String
+    var name_ja: String
+    var deaths: Int
+}
+
+class observer : ObservableObject{
+    @Published var datas = [datatype]()
+    init(){
+        AF.request("https://covid19-japan-web-api.now.sh/api//v1/prefectures")
+            .responseJSON { response in
+                switch response.result{
+                case .success(let value):
+                    let json = JSON(value)
+                    for(key,subJson):(String, JSON) in json{
+                        debugPrint("============================")
+                        debugPrint(subJson["name_ja"])
+                        debugPrint("============================")
+                        debugPrint("key: \(key) subJson: \(subJson)")
+                        self.datas.append(datatype(
+                            id: key,
+                            name_ja:subJson["name_ja"].stringValue,
+                            deaths:subJson["deaths"].intValue))
+                    }
+                case .failure(let error):
+                    debugPrint(error)
+                }
+                
+        }
     }
 }
